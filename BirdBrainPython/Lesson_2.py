99b154f09e9a90b3b7e06789c39468982b6308e1
Jalen Madison <jalenmadison@TheHiddsProject.attlocal.net>
2022-04-25 18:57:42 -0700

    updated lesson 2

--- BirdBrainPython/Lesson_2.py
+++ BirdBrainPython/Lesson_2.py
@@ -3,29 +3,29 @@ import time
 
 bird = Finch()
 
-def exercise1Example():
+def exercise1Example(): #prints the distance the finches sensors see
     print(bird.getDistance())
 
 def exercise1(): #longest distance I got is 106 cm, shortest is 2 cm
     print("Distance: ", bird.getDistance())
 
-def exercise2():
+def exercise2(): #if button A is pressed print true
     bird.getButton('A')
     print(bird.getButton('A'))
 
-def exercise4():
+def exercise4(): #find the distance between the finch and obstacle, then print distance, x2 and x4
     currentDistance = bird.getDistance()
     print(currentDistance)
     print(2*currentDistance)
     print(4*currentDistance)
 
-def exercise5():
+def exercise5(): # get both light values from each light sensor, and print the difference between the two
     leftSensor = bird.getLight('L')
     rightSensor = bird.getLight('R')
     difference = leftSensor - rightSensor
     print(difference)
 
-def exercise6():
+def exercise6(): # get both light values and print the average value
     leftSensor = bird.getLight('L')
     rightSensor = bird.getLight('R')
     mean = leftSensor + rightSensor / 2
